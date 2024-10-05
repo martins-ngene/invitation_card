e15d4bc88a7204027012a0a1c1443a7236d6b6e6
Martins Ngene <martinsngene9@gmail.com>
2024-10-05 07:59:18 +0100

    Adjust meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:title" content="Yomanuel 2024" />
-        <meta property="og:description" content="Yoma Weds Emmanuel" />
-        <meta property="og:site_name" content="https://yomanuel.com" />
+        <meta name="title" property="og:title" content="Yomanuel 2024" />
+        <meta
+          name="description"
+          property="og:description"
+          content="Yoma Weds Emmanuel"
+        />
+        <meta
+          name="site_name"
+          property="og:site_name"
+          content="https://yomanuel.com"
+        />
+        <meta name="type" property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="/opengraph-image.png" />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://drive.google.com/uc?export=view&id=1HtwtYIZlB9jPrOeZ1jGXagpzPXwipxZG"
+        />
       </Head>
       <body className="antialiased">
         <Main />
